a216c6bafa2bd3624d4c38d3a56b34edfb81190b
eveisfalling <evelyn.calderon22@kzoo.edu>
2025-11-05 15:16:14 -0500

    basic overhaul, rooms created, unfinished camera system

---
+++ scenes/camera_2d.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+
+# Takes the player and the size of the screen as variables.
+@export var player: Player
+@export var size: Vector2i = get_viewport_rect().size
+
+# Updates the camera position when the scene is initialized.
+func _ready() -> void:
+	update_position()
+
+# Updates the camera position when the player moves out of its range.
+func _physics_process(_delta) -> void:
+	update_position()
+
+# Updates the camera angle by moving it to the player's position.
+# This is calculated by taking the player's position onscreen divided by the viewport,
+# this is then multiplied by the size of the camera itself.
+func update_position() -> void:
+	var current_cell: Vector2i = Vector2i(player.global_position) / size
+	global_position = current_cell * size
