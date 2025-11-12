6184ccc4ccaf448efc10c299a25f450343f7a957
eveisfalling <evelyn.calderon22@kzoo.edu>
2025-11-12 14:53:38 -0500

    minor changes

--- scenes/musicplayer.gd
+++ scenes/musicplayer.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-#I literally don't know what this does
+#I literally don't know what this does it's soo complicated
 func _on_body_entered(_body: Node2D) -> void:
 	$AudioStreamPlayer.play()
 
